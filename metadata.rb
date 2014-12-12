c8b3d2028e70c9d248a72dade769ad6695f7412f
ubuntu <davidmontgomery@gmail.com>
2014-12-12 11:14:38 +0800

    test

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "riak"
+maintainer       "Rtb"
+maintainer_email "nobdy"
+license          "Apache 2.0"
+description      "Installs riak"
+version          "1.0.0"
+%w{ubuntu}.each do |os|
+  supports os
+end
