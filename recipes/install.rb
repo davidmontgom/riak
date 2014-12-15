e296c0396efc7fb3b78425fe23084b7156bf233c
David Montgomery <davidmontgomery@gmail.com>
2014-12-16 01:51:52 +0800

    test

--- recipes/install.rb
+++ recipes/install.rb
@@ -74,11 +74,13 @@ end
 
 
 
+
 remote_file "#{Chef::Config[:file_cache_path]}/riak_2.0.2-1_amd64.deb" do
   user "root"
   source "http://s3.amazonaws.com/downloads.basho.com/riak/2.0/2.0.2/ubuntu/#{platform_name}/riak_2.0.2-1_amd64.deb"     
   action :create_if_missing
 end
+
 dpkg_package "riak_deb" do
   source "#{Chef::Config[:file_cache_path]}/riak_2.0.2-1_amd64.deb"
   action :install
@@ -87,6 +89,23 @@ end
 service "riak" do
   supports :restart => true,:start => true, :stop => true
   action [:start,:enable]
+end
+
+
+execute "riak-restart" do
+  command "riak stop && riak start"
+  action :nothing
+end
+   
+template "/etc/riak/riak.conf" do
+  path "/etc/riak/riak.conf"
+  source "riak-#{node['platform_version']}.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  variables :enviroment => node.chef_environment, :ipaddress => ipaddress
+  notifies :run, "execute[riak-restart]", :delayed
+  #notifies :restart, resources(:service => "riak"), :immediately
 end
 
 
